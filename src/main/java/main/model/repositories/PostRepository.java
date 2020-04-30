27818e0bc9fa051271468536c62c97ad6c6340f8
Zhusupov Marat <zmaratzz@yandex.ru>
2020-04-30 20:24:30 +0300

    changed time

--- src/main/java/main/model/repositories/PostRepository.java
+++ src/main/java/main/model/repositories/PostRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
     @Query(value = "SELECT * FROM posts WHERE is_active = 1 AND moderation_status = 'ACCEPTED'" +
-            " AND time > current_time ORDER BY time LIMIT :limit OFFSET :offset", nativeQuery = true)
+            " AND time < current_time ORDER BY time LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<Post> findPostByDateAsc (@Param("offset") int offset, @Param("limit") int limit);
     @Query(value = "SELECT * FROM posts WHERE is_active = 1 AND moderation_status = 'ACCEPTED'" +
-            " AND time > current_time ORDER BY time DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+            " AND time < current_time ORDER BY time DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<Post> findPostByDateDesc (@Param("offset") int offset, @Param("limit") int limit);
     @Query(value = "SELECT * FROM posts WHERE is_active = 1 AND moderation_status = 'ACCEPTED'" +
-            " AND time > current_time LIMIT :limit OFFSET :offset", nativeQuery = true)
+            " AND time < current_time LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<Post> findPost (@Param("offset") int offset, @Param("limit") int limit);
 }
